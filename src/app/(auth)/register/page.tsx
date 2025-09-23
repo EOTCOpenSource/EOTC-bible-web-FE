2049fa0a9cc53cf94103dbe9a587afb75d9bce0b
henaorth16 <emyayehenok@gmail.com>
2025-09-24 00:32:02 +0300

    update login ui

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function RegisterPage() {
   return (
-      <div className=" md:flex md-w-full top-4 left-0 rounded-3xl bg-gray-100 p-2 md:p-11">
-        <div className="image-box md:w-[60%] sm:max-h-[60vh] max-h-[40vh] md:max-h-[90vh] max-w-[50%] rounded-2xl overflow-hidden">
+      <div className=" md:flex md:items-center md-w-full top-4 left-0 md:rounded-3xl bg-gray-100 p-2 md:p-11">
+        <div className="image-box md:w-[60%] sm:max-h-[60vh] max-h-[40vh] md:max-h-[90vh] md:max-w-[50%] rounded-2xl overflow-hidden">
           <img
             src={
               "https://images.pexels.com/photos/17852030/pexels-photo-17852030.jpeg"
@@ -13,7 +13,7 @@ export default function RegisterPage() {
             alt="woman-in-white-robes-and-with-candle"
           />
         </div>
-        <div className="form-box md:px-4">
+        <div className="form-box md:px-4 mx-auto">
           <RegisterForm />
         </div>
       </div>
--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,90 +1,111 @@
-"use client"
+"use client";
 
-import type React from "react"
-import { useAuthStore } from "@/lib/stores/useUserStore"
-import { useRouter } from "next/navigation"
-import { useState } from "react"
-import axios from "axios"
-import { useForm } from "react-hook-form"
+import type React from "react";
+import { useAuthStore } from "@/lib/stores/useUserStore";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 
 type FormData = {
-  email: string
-  password: string
-}
+  email: string;
+  password: string;
+};
 
 export default function LoginForm() {
-  const { loadSession } = useAuthStore()
-  const router = useRouter()
-  const [err, setErr] = useState<string | null>(null)
-  const [loading, setLoading] = useState(false)
+  const { loadSession } = useAuthStore();
+  const router = useRouter();
+  const [err, setErr] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>()
+  } = useForm<FormData>();
 
   const onSubmit = async (data: FormData) => {
-    setErr(null)
-    setLoading(true)
+    setErr(null);
+    setLoading(true);
 
     try {
       const res = await axios.post("/api/auth/login", data, {
         headers: { "Content-Type": "application/json" },
         withCredentials: true,
-      })
+      });
 
       if (!res.data) {
-        setErr("Empty response from server")
-        return
+        setErr("Empty response from server");
+        return;
       }
 
-      await loadSession()
-      router.push("/dashboard")
+      await loadSession();
+      router.push("/dashboard");
     } catch (error: any) {
       if (error.response) {
-        setErr(error.response.data?.error || "Login failed")
+        setErr(error.response.data?.error || "Login failed");
       } else {
-        setErr(error.message || "Login failed")
+        setErr(error.message || "Login failed");
       }
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="max-w-sm space-y-4">
-      <input
-        className="w-full border p-3 rounded-lg"
-        placeholder="Email"
-        type="email"
-        {...register("email", { required: "Email is required" })}
-      />
-      {errors.email && <p className="text-red-600 text-sm">{errors.email.message}</p>}
-
-      <input
-        className="w-full border p-3 rounded-lg"
-        placeholder="Password"
-        type="password"
-        {...register("password", { required: "Password is required" })}
-      />
-      {errors.password && <p className="text-red-600 text-sm">{errors.password.message}</p>}
-
-      <p>
-        you don't have account yet?{" "}
-        <a className="inline text-blue-500" href="/register">
-          register
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 p-4">
+      <h2 className="text-2xl font-semibold py-0 my-0">Welcome Back!</h2>
+      <p className="text-sm text-gray-600 mb-4">
+        Dont have an account yet?{" "}
+        <a className="text-blue-500 underline" href="/register">
+          Signup
         </a>
-      </p>
-
+      </p>{" "}
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          className="w-full border p-3 rounded-lg"
+          placeholder="Email"
+          id="email"
+          type="email"
+          {...register("email", { required: "Email is required" })}
+        />
+      </div>
+      {errors.email && (
+        <p className="text-red-600 text-sm">{errors.email.message}</p>
+      )}
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          className="w-full border p-3 rounded-lg"
+          placeholder="Password"
+          type="password"
+          id="password"
+          {...register("password", { required: "Password is required" })}
+        />
+      </div>
+      {errors.password && (
+        <p className="text-red-600 text-sm">{errors.password.message}</p>
+      )}
+      
       {err && <p className="text-red-600 text-sm">{err}</p>}
-
       <button
-        className="w-full bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 disabled:opacity-50"
+        className="w-full bg-[#621B1C] text-white p-3 rounded-lg hover:bg-[#471314] cursor-pointer disabled:opacity-50"
         disabled={loading}
       >
         {loading ? "Signing in..." : "Sign in"}
       </button>
+      <div className="flex items-center gap-4 my-2">
+        <div className="flex-1 border-t border-gray-300"></div>
+        <span className="text-gray-500 text-sm">OR</span>
+        <div className="flex-1 border-t border-gray-300"></div>
+      </div>
+      <button
+        className="w-full flex justify-center items-center gap-2 bg-[#c9c9c9] cursor-pointer text-gray-700 p-2 rounded-lg hover:bg-gray-400 disabled:bg-gray-400"
+        disabled={loading}
+      >
+       <img className="w-[30px]" src="https://hackaday.com/wp-content/uploads/2016/08/google-g-logo.png" alt="google logo  image" /> {loading ? "..." : "Continue with Google"}
+      </button>
     </form>
-  )
+  );
 }
