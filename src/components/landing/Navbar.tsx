d075b7280863586656021a71094131243578ab65
Dagim Asrat <dagimasrat3@gmail.com>
2025-10-04 11:51:21 +0300

    fix: fix conflicts occured during pr #10 Landing

--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Search, Moon, Globe, User, Menu, X, ArrowUpRight } from "lucide-react";
 import Link from "next/link";
 import { useUIStore } from "@/stores/uiStore";
+import { Button } from "../ui/button";
 
 const Navbar = () => {
   const {
@@ -23,7 +24,9 @@ const Navbar = () => {
               <span className="font-bold text-xl">EOTCBible</span>
             </div>
             <div className="hidden md:flex items-center space-x-8">
-              <Button asChild variant={"outline"}
+              <Button
+                asChild
+                variant={"outline"}
                 // className="text-black hover:text-gray-900"
               >
                 <Link href="#">Bible</Link>
