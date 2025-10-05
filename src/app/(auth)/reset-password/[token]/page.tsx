bf2a0f1933b0f0631bc2a1ee09ba132f05fefec5
henaorth16 <emyayehenok@gmail.com>
2025-10-06 01:48:48 +0300

    chore(reset-password): add password measurement and eye icon for password visibility

--- src/app/(auth)/reset-password/[token]/page.tsx
+++ src/app/(auth)/reset-password/[token]/page.tsx
@@ -5,6 +5,7 @@ import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useState } from "react";
+import { DotIcon, Eye, EyeOff } from "lucide-react";
 
 type ResetPasswordForm = {
   newPassword: string;
@@ -16,7 +17,8 @@ export default function ResetPasswordPage() {
   const router = useRouter();
   const [message, setMessage] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
-
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [showNewPassword, setShowNewPassword] = useState(false);
   const {
     register,
     handleSubmit,
@@ -24,6 +26,21 @@ export default function ResetPasswordPage() {
     formState: { errors },
     reset,
   } = useForm<ResetPasswordForm>();
+
+  const passwordValue = watch("newPassword");
+
+  const passwordCriteria = [
+    {
+      label: "At least 1 Uppercase letter",
+      valid: /[A-Z]/.test(passwordValue || ""),
+    },
+    { label: "At least 1 Number", valid: /\d/.test(passwordValue || "") },
+    {
+      label: "At least 1 special character",
+      valid: /[!@#$%^&*]/.test(passwordValue || ""),
+    },
+    { label: "Minimum 8 characters", valid: passwordValue?.length >= 8 },
+  ];
 
   const onSubmit = async (data: ResetPasswordForm) => {
     setIsSubmitting(true);
@@ -38,7 +55,7 @@ export default function ResetPasswordPage() {
       setMessage(res.data.message);
 
       if (res.data.success) {
-        reset(); 
+        reset();
         setTimeout(() => router.push("/login"), 600);
       }
     } catch (error: any) {
@@ -53,7 +70,9 @@ export default function ResetPasswordPage() {
   return (
     <section className="flex min-h-screen items-center justify-center p-4">
       <div className="w-full max-w-md rounded-xl bg-gray-300 p-8 shadow-lg">
-        <h1 className="text-2xl text-center font-bold">Create a new password</h1>
+        <h1 className="text-2xl text-center font-bold">
+          Create a new password
+        </h1>
         <p className="text-center text-sm text-gray-700">
           Please enter a new password for your EOTCBible account.
         </p>
@@ -70,18 +89,28 @@ export default function ResetPasswordPage() {
             >
               New password
             </label>
-            <Input
-              type="password"
-              id="newPassword"
-              className="border border-gray-500 p-2 rounded"
-              {...register("newPassword", {
-                required: "New password is required",
-                minLength: {
-                  value: 6,
-                  message: "Password must be at least 6 characters",
-                },
-              })}
-            />
+            <div className="relative">
+              <Input
+                type={showNewPassword ? "text" : "password"}
+                id="newPassword"
+                className="border border-gray-500 p-2 rounded"
+                {...register("newPassword", {
+                  required: "New password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters",
+                  },
+                })}
+              />
+              <button
+                type="button"
+                onClick={() => setShowNewPassword(!showNewPassword)}
+                className="absolute right-3 top-2.5  text-gray-600 cursor-pointer"
+              >
+                {showNewPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+              </button>
+              
+            </div>
             {errors.newPassword && (
               <p className="text-red-600 text-sm">
                 {errors.newPassword.message}
@@ -97,22 +126,44 @@ export default function ResetPasswordPage() {
             >
               Confirm password
             </label>
-            <Input
-              type="password"
-              id="confirmPassword"
-              className="border p-2 rounded"
-              {...register("confirmPassword", {
-                required: "Confirm password is required",
-                validate: (val) =>
-                  val === watch("newPassword") || "Passwords do not match",
-              })}
-            />
+
+            <div className="relative">
+              <Input
+                type={showConfirmPassword ? "text" : "password"}
+                id="confirmPassword"
+                className="border p-2 rounded"
+                {...register("confirmPassword", {
+                  required: "Confirm password is required",
+                  validate: (val) =>
+                    val === watch("newPassword") || "Passwords do not match",
+                })}
+              />
+              <button
+                type="button"
+                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                className="absolute right-3 top-2.5  text-gray-600 cursor-pointer"
+              >
+                {showConfirmPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+              </button>
+            </div>
+
             {errors.confirmPassword && (
               <p className="text-red-600 text-sm">
                 {errors.confirmPassword.message}
               </p>
             )}
           </div>
+          <ul className="text-sm flex flex-wrap  items-center mb-1">
+            {passwordCriteria.map((c, i) => (
+              <li
+                key={i}
+                className={c.valid ? "text-green-600" : "text-gray-500"}
+              >
+                {/* {c.valid ? "✔" : "✖"} */}
+                <DotIcon className="inline -mr-2" /> {c.label}
+              </li>
+            ))}
+          </ul>
           <Button
             type="submit"
             disabled={isSubmitting}
