0cf4c92dd81001e2a219f5d24766632d4a2a5776
eyob13-coder <eyobgeremew618@gmail.com>
2025-10-17 00:02:47 +0300

    updated VerseActionMenu

---
+++ src/components/layout/LandingLayout.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+export const LandingLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="min-h-screen bg-background text-foreground">
+    {children}
+  </div>
+}
